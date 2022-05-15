7791d3367fa70bcc6a21890ba983d053b8c2908a
guonan <838093891@qq.com>
2022-05-15 13:58:56 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: './src/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: path.resolve(__dirname, './loader/strReplaceLoader.js'), // 'babel-loader',
+            options: {
+              chiniseName: '南'
+            }
+          }
+        ]
+      },
+    ]
+  }
+}
