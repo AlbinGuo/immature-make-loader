2719be67292cc8ebfd7a087ced49d89072e236de
guonan <838093891@qq.com>
2022-05-15 21:46:44 +0800

    add: 异步loader

--- loader/strReplaceLoader.js
+++ loader/strReplaceLoader.js
@@ -4,6 +4,13 @@ module.exports = function(source) {
   const options = getOptions(this)
   // return source.replace('guo', options.chiniseName)            // 使用return只能返回一个参数
   this.callback(null, source.replace('guo', options.chiniseName)) // 使用callback方式，可以返回多个参数
+
+  // 异步loader编写
+  // const callback = this.async()
+  // setTimeout(() => {
+  //   callback(null, source.replace('guo', options.chiniseName))
+  // }, 1000)
+
 }
 
 // this.callback({
